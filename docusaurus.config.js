d859400a59955bfcb05614554aa1998f5e8d05d7
Caleb A. Collar <49846301+Mindstormer-0@users.noreply.github.com>
2022-12-07 02:37:31 -0600

    Remove subpath for url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: "daq-tool docs",
   tagline: "daq-tool docs!",
-  url: "https://mindstormer-0.github.io/daq-tool/",
+  url: "https://mindstormer-0.github.io",
   baseUrl: "/daq-tool/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
